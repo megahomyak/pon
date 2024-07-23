ce34047ed820b382c80145289dca77fb9f0e973e
megahomyak <g.megahomyak@gmail.com>
2024-07-24 01:15:07 +0500

    Trying to rewrite it "in a right way", should instead focus on the concept of states

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,34 @@
-mod parser {
+type Input<'a> = std::str::CharIndices<'a>;
 
+struct Pos { index: usize }
+enum Result<'a, O, E> {
+    Success { output: O, rest: Input<'a> },
+    Failure { error: E, span: std::ops::RangeInclusive<Pos> }
+}
+use Result::*;
+
+mod word_char {
+    use super::*;
+
+    pub enum Error {
+        Whitespace(char),
+        Semicolon(),
+        EscapeAtEnd(),
+
+    }
+
+    pub fn parse(s: Input) -> Result<char, Error> {
+        use Error::*;
+
+        match s.next() {
+            
+        }
+
+        match  {
+            '\\' => match
+            ';' => Err(Semicolon),
+            c if c.is_whitespace() => Success(Whitespace(c)),
+            c => Success(WordChar(c)),
+        }
+    }
 }
