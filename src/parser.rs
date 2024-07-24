3687d07238bd8acd25719ee6c034be8f210479e3
megahomyak <g.megahomyak@gmail.com>
2024-07-25 02:16:49 +0500

    Finished it.

--- src/parser.rs
+++ src/parser.rs
@@ -1,254 +1,197 @@
-mod input {
-    pub struct Input<'a> {
-        current_index: usize,
-        content: &'a str,
-    }
-    impl<'a> Input<'a> {
-        pub fn new(s: &'a str) -> Self {
-            Self {
-                content: s,
-                current_index: 0,
-            }
-        }
-        pub fn next(&mut self) -> Option<char> {
-            unsafe { self.content.get_unchecked(self.current_index..) }
-                .chars()
-                .next()
-                .inspect(|c| self.current_index += c.len_utf8())
-        }
-        pub fn content(&self) -> &str {
-            self.content
-        }
-        pub fn current_index(&self) -> usize {
-            self.current_index
-        }
+#[derive(Clone)]
+pub struct ParserInput<'a> {
+    idx: usize,
+    s: &'a str,
+}
+impl<'a> ParserInput<'a> {
+    fn next(&mut self) -> Option<char> {
+        unsafe { self.s.get_unchecked(self.idx..) }
+            .chars()
+            .next()
+            .inspect(|c| self.idx += c.len_utf8())
     }
 }
-use input::Input;
 
-struct WordSeparator(char);
-struct CommandInvocationSeparator(char);
-struct InputOpener(char);
-struct InputCloser(char);
-
-enum InvalidInput {
+pub struct Word(pub String);
+enum AfterWord {
+    CommandSeparator(),
+    WordSeparator(),
+    PonInputOpener(),
+    ParserInputEnd(),
     EscapeAtEndOfInput(),
 }
-use InvalidInput as II_;
-
-enum Result<Success, UnexpectedInput> {
-    Success(Success),
-    UnexpectedInput(UnexpectedInput),
-    InvalidInput(InvalidInput),
-}
-use Result::InvalidInput as II;
-use Result::Success as S;
-use Result::UnexpectedInput as UI;
-
-mod program {
-    use super::*;
-    mod command_invocation {
-        use super::*;
-        mod command_name {
-            use super::*;
-            mod word {
-                use super::*;
-                mod char_ {
-                    use super::*;
-                    struct Char(char);
-                    enum UnexpectedInput {
-                        WordSeparator(WordSeparator),
-                        CommandInvocationSeparator(CommandInvocationSeparator),
-                        InputOpener(InputOpener),
-                        InputCloser(InputCloser),
-                        InputEnd(),
-                    }
-                    use UnexpectedInput as UI_;
-                    fn parse(s: &mut Input) -> Result<Char, UnexpectedInput> {
-                        match s.next() {
-                            None => UI(UI_::InputEnd()),
-                            Some(c @ ';') => UI(UI_::CommandInvocationSeparator(
-                                CommandInvocationSeparator(c),
-                            )),
-                            Some(c @ '(') => UI(UI_::InputOpener(InputOpener(c))),
-                            Some(c @ ')') => UI(UI_::InputCloser(InputCloser(c))),
-                            Some('\\') => match s.next() {
-                                None => II(InvalidInput::EscapeAtEndOfInput()),
-                                Some(c) => S(Char(c)),
-                            },
-                            Some(c) if c.is_whitespace() => {
-                                UI(UI_::WordSeparator(WordSeparator(c)))
-                            }
-                            Some(c) => S(Char(c)),
-                        }
-                    }
-                }
-            }
-        }
-        use super::*;
-        mod command_input {
-            use super::*;
-            mod contents {
-                use super::*;
-                mod char_ {
-                    use super::*;
-                }
-            }
-        }
-    }
-
-    enum ParsingResult {}
-
-    pub fn parse(s: Input) -> ParsingResult {}
-}
-
-struct CommandNameWord(Vec<CommandNameWordChar>);
-enum CommandNameWordParsingResult {
-    // Unexpected input
-    Whitespace(Whitespace, Option<CommandNameWord>),
-    Semicolon(Semicolon, Option<CommandNameWord>),
-    OpeningParen(OpeningParen, Option<CommandNameWord>),
-    ClosingParen(ClosingParen, Option<CommandNameWord>),
-    Nothing(Option<CommandNameWord>),
-    // Invalid input
-    EscapeAtEnd(),
-}
-fn parse_word(s: &mut Input) -> CommandNameWordParsingResult {
-    use CommandNameWordCharParsingResult as I;
-    use CommandNameWordParsingResult as O;
-    let mut word = Vec::new();
-    let pack = move |mut word: Vec<CommandNameWordChar>| {
-        if word.is_empty() {
-            None
-        } else {
-            word.shrink_to_fit();
-            Some(CommandNameWord(word))
-        }
-    };
-    loop {
-        match parse_word_char(s) {
-            I::Nothing() => return O::Nothing(pack(word)),
-            I::EscapeAtEnd() => return O::EscapeAtEnd(),
-            I::Valid(c) => word.push(c),
-            I::Semicolon(c) => return O::Semicolon(c, pack(word)),
-            I::OpeningParen(c) => return O::OpeningParen(c, pack(word)),
-            I::Whitespace(c) => return O::Whitespace(c, pack(word)),
-            I::ClosingParen(c) => return O::ClosingParen(c, pack(word)),
-        }
-    }
-}
-
-struct CommandName(Vec<CommandNameWord>);
-enum NameParsingResult {
-    // Unexpected input
-    Semicolon(Semicolon, Option<CommandName>),
-    OpeningParen(OpeningParen, Option<CommandName>),
-    ClosingParen(ClosingParen, Option<CommandName>),
-    Nothing(Option<CommandName>),
-    // Invalid input
-    EscapeAtEnd(),
-}
-fn parse_name(s: &mut Input) -> NameParsingResult {
-    use CommandNameWordParsingResult as I;
-    use NameParsingResult as O;
-    let mut words = Vec::new();
-    let mut maybe_add = |word: Option<CommandNameWord>| {
-        if let Some(w) = word {
-            words.push(w);
-        }
-    };
-    let pack = |mut words: Vec<CommandNameWord>| {
-        if words.is_empty() {
-            None
-        } else {
-            words.shrink_to_fit();
-            Some(CommandName(words))
-        }
-    };
-    loop {
-        match parse_word(s) {
-            I::EscapeAtEnd() => return O::EscapeAtEnd(),
-            I::Nothing(w) => {
-                maybe_add(w);
-                return O::Nothing(pack(words));
-            }
-            I::ClosingParen(c, w) => {
-                maybe_add(w);
-                return O::ClosingParen(c, pack(words));
-            }
-            I::OpeningParen(c, w) => {
-                maybe_add(w);
-                return O::OpeningParen(c, pack(words));
-            }
-            I::Semicolon(c, w) => {
-                maybe_add(w);
-                return O::Semicolon(c, pack(words));
-            }
-            I::Whitespace(_c, w) => {
-                maybe_add(w);
-            }
-        }
-    }
-}
-
-enum InputChar {
-    // Success
-    Valid(char),
-    ValidEscaped(char, char),
-    // Unexpected input
-    OpeningParen(char),
-    ClosingParen(char),
-    // Invalid input
-    EscapeAtEnd,
-    Nothing,
-}
-impl InputChar {
-    fn parse(s: &mut Input) -> Self {
+fn word(s: &mut ParserInput) -> (Option<Word>, AfterWord) {
+    let mut word = String::new();
+    let after = loop {
         match s.next() {
-            None => Self::Nothing,
-            Some(c @ '(') => Self::OpeningParen(c),
-            Some(c @ ')') => Self::ClosingParen(c),
-            Some(c1 @ '\\') => match s.next() {
-                None => Self::EscapeAtEnd,
-                Some(c2) => Self::ValidEscaped(c1, c2),
-            },
-            Some(c) => Self::Valid(c),
-        }
-    }
-}
-
-enum InputContents {
-    // Unexpected input
-    ClosingParen(char, String),
-    // Invalid input
-    EscapeAtEnd,
-    Nothing,
-}
-impl InputContents {
-    fn parse(s: &mut Input) -> Self {
-        let mut chars = String::new();
-        let mut nesting_level = 0;
-        loop {
-            match InputChar::parse(s) {
-                InputChar::Nothing => return Self::Nothing,
-                InputChar::EscapeAtEnd => return Self::EscapeAtEnd,
-                InputChar::Valid(c) => chars.push(c),
-                InputChar::ValidEscaped(c1, c2) => {
-                    chars.push(c1);
-                    chars.push(c2);
+            None => break AfterWord::ParserInputEnd(),
+            Some(mut c) => {
+                match c {
+                    '(' => break AfterWord::PonInputOpener(),
+                    ';' | '\n' => break AfterWord::CommandSeparator(),
+                    '\\' => match s.next() {
+                        None => break AfterWord::EscapeAtEndOfInput(),
+                        Some(escaped_c) => c = escaped_c,
+                    },
+                    _ if c.is_whitespace() => break AfterWord::WordSeparator(),
+                    _ => (),
                 }
-                InputChar::OpeningParen(c) => {
-                    nesting_level += 1;
-                    chars.push(c);
-                }
-                InputChar::ClosingParen(c) => {
-                    if nesting_level == 0 {
-                        return Self::ClosingParen(c, chars);
-                    }
-                    nesting_level -= 1;
-                    chars.push(c);
-                }
+                word.push(c);
             }
         }
-    }
+    };
+    let word = if word.is_empty() {
+        None
+    } else {
+        word.shrink_to_fit();
+        Some(Word(word))
+    };
+    (word, after)
+}
+
+pub struct Name(pub Vec<Word>);
+enum AfterName {
+    CommandSeparator(),
+    PonInputOpener(),
+    ParserInputEnd(),
+    EscapeAtEndOfInput(),
+}
+fn name(s: &mut ParserInput) -> (Option<Name>, AfterName) {
+    let mut name = Vec::new();
+    let after = loop {
+        let (word, after_word) = word(s);
+        if let Some(word) = word {
+            name.push(word);
+        }
+        match after_word {
+            AfterWord::EscapeAtEndOfInput() => break AfterName::EscapeAtEndOfInput(),
+            AfterWord::CommandSeparator() => break AfterName::CommandSeparator(),
+            AfterWord::PonInputOpener() => break AfterName::PonInputOpener(),
+            AfterWord::ParserInputEnd() => break AfterName::ParserInputEnd(),
+            AfterWord::WordSeparator() => (),
+        }
+    };
+    let name = if name.is_empty() {
+        None
+    } else {
+        name.shrink_to_fit();
+        Some(Name(name))
+    };
+    (name, after)
+}
+
+pub struct PonInput(pub String);
+enum AfterPonInput {
+    PonInputTerminator(),
+    ParserInputEnd(),
+    EscapeAtEndOfInput(),
+}
+fn pon_input(s: &mut ParserInput) -> (PonInput, AfterPonInput) {
+    let mut input = String::new();
+    let mut nesting_level = 0;
+    let after = loop {
+        match s.next() {
+            None => break AfterPonInput::ParserInputEnd(),
+            Some(mut c) => {
+                match c {
+                    ')' => {
+                        if nesting_level == 0 {
+                            break AfterPonInput::PonInputTerminator();
+                        }
+                        nesting_level -= 1;
+                    }
+                    '(' => nesting_level += 1,
+                    '\\' => match s.next() {
+                        None => break AfterPonInput::EscapeAtEndOfInput(),
+                        Some(escaped_c) => {
+                            input.push(c);
+                            c = escaped_c;
+                        }
+                    },
+                    _ => (),
+                }
+                input.push(c);
+            }
+        }
+    };
+    input.shrink_to_fit();
+    (PonInput(input), after)
+}
+
+pub struct Command(pub Option<Name>, pub Vec<PonInput>);
+enum AfterCommand {
+    CommandSeparator(),
+    MissingInputTerminator(),
+    EscapeAtEndOfInput(),
+    ParserInputEnd(),
+}
+pub fn command(s: &mut ParserInput) -> (Option<Command>, AfterCommand) {
+    use name as parse_name;
+    let (name, after_name) = parse_name(s);
+    let mut pon_inputs = Vec::new();
+    let after = loop {
+        match after_name {
+            AfterName::ParserInputEnd() => break AfterCommand::ParserInputEnd(),
+            AfterName::PonInputOpener() => {
+                break loop {
+                    let (pon_input, after_pon_input) = pon_input(s);
+                    match after_pon_input {
+                        AfterPonInput::ParserInputEnd() => break AfterCommand::MissingInputTerminator(),
+                        AfterPonInput::EscapeAtEndOfInput() => {
+                            break AfterCommand::EscapeAtEndOfInput()
+                        }
+                        AfterPonInput::PonInputTerminator() => (),
+                    }
+                    pon_inputs.push(pon_input);
+                    let mut new_s = s.clone();
+                    let (name, after_name) = parse_name(&mut new_s);
+                    if name.is_some() {
+                        break AfterCommand::CommandSeparator();
+                    }
+                    match after_name {
+                        AfterName::EscapeAtEndOfInput() => {
+                            break AfterCommand::EscapeAtEndOfInput()
+                        }
+                        AfterName::PonInputOpener() => (),
+                        AfterName::ParserInputEnd() => break AfterCommand::ParserInputEnd(),
+                        AfterName::CommandSeparator() => break AfterCommand::CommandSeparator(),
+                    }
+                    *s = new_s;
+                }
+            }
+            AfterName::CommandSeparator() => break AfterCommand::CommandSeparator(),
+            AfterName::EscapeAtEndOfInput() => break AfterCommand::EscapeAtEndOfInput(),
+        }
+    };
+    let command = if name.is_none() && pon_inputs.is_empty() {
+        None
+    } else {
+        pon_inputs.shrink_to_fit();
+        Some(Command(name, pon_inputs))
+    };
+    (command, after)
+}
+
+pub struct Program(Vec<Command>);
+pub enum AfterProgram {
+    EscapeAtEndOfInput(),
+    ParserInputEnd(),
+    MissingInputTerminator(),
+}
+pub fn program(s: &mut ParserInput) -> (Program, AfterProgram) {
+    let mut commands = Vec::new();
+    let after = loop {
+        let (command, after_command) = command(s);
+        if let Some(command) = command {
+            commands.push(command);
+        }
+        match after_command {
+            AfterCommand::CommandSeparator() => (),
+            AfterCommand::EscapeAtEndOfInput() => break AfterProgram::EscapeAtEndOfInput(),
+            AfterCommand::ParserInputEnd() => break AfterProgram::ParserInputEnd(),
+            AfterCommand::MissingInputTerminator() => break AfterProgram::MissingInputTerminator(),
+        }
+    };
+    commands.shrink_to_fit();
+    (Program(commands), after)
 }
