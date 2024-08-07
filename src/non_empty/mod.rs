bd5442e65810ccc3e60afac36f8d8e7052bcd651
megahomyak <g.megahomyak@gmail.com>
2024-08-08 00:54:33 +0300

    This fucking programming language is KILLING ME

--- src/non_empty/mod.rs
+++ src/non_empty/mod.rs
@@ -1,15 +1,7 @@
-use crate::parser::parser_input::part::Part;
 use crate::parser::parser_input::Input;
 
-#[derive(Eq, PartialEq)]
+#[derive(Eq, PartialEq, Debug)]
 pub struct NonEmpty<I: Input> {
     pub first: I::PartContent,
     pub rest: I::PartContainer,
 }
-
-impl<I: Input> PartialEq for NonEmpty<I> {
-    fn eq(&self, other: &Self) -> bool {
-        self.first == other.first && self.rest == other.rest
-    }
-}
-impl<I: Input> Eq for NonEmpty<I> {}
